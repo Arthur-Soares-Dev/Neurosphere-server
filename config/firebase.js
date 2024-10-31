646c22c30ce39838dd94db72099f69bfa3888bb2
Arthur-Soares-Dev <arthursoaresdev@gmail.com>
2024-10-31 20:49:52 -0300

    segundo commit

--- config/firebase.js
+++ config/firebase.js
@@ -1,14 +1,15 @@
-// /backend/config/firebase.js
 const admin = require('firebase-admin');
+require('dotenv').config();
 
-// Chave do Firebase Admin SDK
-const serviceAccount = require('./serviceAccount');
-
-// Inicialize o Firebase Admin SDK
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "https://neurosphere-132f0.firebaseapp.com",
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
+  storageBucket: process.env.STORAGE_BUCKET
 });
+
 const firestore = admin.firestore();
 const auth = admin.auth();
 const storage = admin.storage().bucket();
