483c1a01dae05dcc9430e14f2f03c8e89f7be999
ARTHUR_SOARES_LOPES_FERREIRA <12201839@ALFAB28.BARROCA>
2024-11-12 11:29:08 -0300

    commit teste refatoracao

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,45 @@
-const { createUserWithAuth, loginUserByEmail, getUser, updateUser, logoutUser } = require('../models/userModel');
+//const { createUserWithAuth, loginUserByEmail, getUser, updateUser, logoutUser } = require('../models/userModel');
 const multer = require('multer');
-const { firestore, storage } = require('../config/firebase');
+const { firestore, storage, auth } = require('../config/firebase');
 const upload = multer({ storage: multer.memoryStorage() }).single('image');
 
 
-// Registrar novo usuário
 async function registerUser(req, res) {
     const { email, password, name } = req.body;
+    console.log('Inicializando a criação do usuário:', email, password, name)
     try {
-        const user = await createUserWithAuth(email, password, name);
-        res.status(201).json(user);
+        const userRecord = await auth.createUser({ email, password });
+        const uid = userRecord.uid;
+
+        const userData = { 
+          name,
+          email
+        };
+      
+      await firestore.collection('users').doc(uid).set(userData);
+
+      const user = { uid, ...userData };
+
+       res.status(201).json(user);
+       console.log('Usuário logado com sucesso')
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
-// Fazer login
 async function login(req, res) {
     const { email, password } = req.body;
+    console.log('Inicializando o login do usuário:', email)
     try {
-        const userData = await loginUserByEmail(email, password);
+        const user = await auth.getUserByEmail(email);
+        const userData = await getUserProfile(user.uid);
         res.status(200).json(userData);
+        console.log('Usuário logado com sucesso')
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
-// Atualizar usuário
 async function updateUserProfile(req, res) {
   const { uid } = req.params;
   const updates = req.body;
@@ -38,7 +51,6 @@ async function updateUserProfile(req, res) {
       console.log('Imagem recebida para o usuário:', uid);
 
       const fileBuffer = req.file.buffer;
-      console.log('Tamanho da imagem:', req.file.size, 'bytes');
 
       const imageName = `profileImages/${uid}_${Date.now()}.jpg`;
       const file = storage.file(imageName);
@@ -59,29 +71,30 @@ async function updateUserProfile(req, res) {
     }
 
     await firestore.collection('users').doc(uid).update(updates);
-    console.log('Campos do perfil atualizados no Firestore.');
 
     const updatedUserDoc = await firestore.collection('users').doc(uid).get();
     res.status(200).json({ uid, ...updatedUserDoc.data() });
     console.log('Dados do usuário retornados com sucesso.');
-
   } catch (error) {
     console.error('Erro ao atualizar o perfil:', error);
     res.status(500).json({ error: error.message });
   }
 }
 
-// Logout
 function logout(req, res) {
-    const message = logoutUser();
+    const message = "Logout bem-sucedido.";
     res.status(200).json(message);
 }
 
-// Buscar perfil de usuário por uid
 async function getUserProfile(req, res) {
     const { uid } = req.params;
     try {
-        const user = await getUser(uid);
+        const userDoc = await firestore.collection('users').doc(uid).get();
+        if (!userDoc.exists) {
+            throw new Error('Usuário não encontrado.');
+        }
+        
+        const user = { uid, ...userDoc.data() };
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ error: error.message });
